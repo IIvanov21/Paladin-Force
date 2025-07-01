a414e1475b4777f69d7d5d2e914221c2ba84f6e2
Ivaylo Ivanov <45109015+IIvanov21@users.noreply.github.com>
2025-07-01 17:11:32 +0100

    network samples built

---
+++ Assets/Scripts/ClientNetworkAnimator.cs
@@ -0,0 +1,10 @@
+using Unity.Netcode.Components;
+using UnityEngine;
+using UnityEngine.Networking;
+public class ClientNetworkAnimator : NetworkAnimator
+{
+    protected override bool OnIsServerAuthoritative()
+    {
+        return false;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,6 @@
-﻿using Unity.Netcode;
+﻿using System;
+using Unity.Collections;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -50,6 +52,47 @@ public class PlayerController : NetworkBehaviour
 
     private Vector2 inputDirection;
 
+    private int playerScore = 0;
+
+    public GameObject spawnedObject;
+
+    NetworkVariable<int> score = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+
+    public struct PlayerCustomData : INetworkSerializable
+    {
+        public ulong id;
+        public float health;
+        public float mana;
+        public FixedString32Bytes name;//When we want to use strings, don't use the direct string class, instead use FixedString32Bytes.
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref id);
+            serializer.SerializeValue(ref health);
+            serializer.SerializeValue(ref mana);
+            serializer.SerializeValue(ref name);
+        }
+    }
+
+    //private NetworkVariable<PlayerCustomData> data;
+
+    public override void OnNetworkSpawn()//OnEnable
+    {
+        if(IsOwner)
+        {
+            //data.Value = new PlayerCustomData { health = 10, mana = 100, id = OwnerClientId, name = $"Player{OwnerClientId}" };
+        }
+        score.OnValueChanged += DisplayScore;
+
+    }
+
+    public override void OnNetworkDespawn()//OnDisable
+    {
+        score.OnValueChanged -= DisplayScore;
+    }
+
+    
+
     private void Update()
     {
         if(IsOwner)
@@ -58,8 +101,21 @@ public class PlayerController : NetworkBehaviour
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
             Vector2 input= new Vector2(horizontal, vertical);
-
+            if (Input.GetKeyDown(KeyCode.P)) SendInfoRpc(UnityEngine.Random.Range(5,100));
             SendInputServerRpc(input);
+
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                spawnedObject=Instantiate(spawnedObject);
+                SpawnObjectRpc();
+            }
+            if(Input.GetKeyDown(KeyCode.I))
+            {
+                spawnedObject.GetComponent<NetworkObject>().Despawn();
+                Destroy(spawnedObject);
+            }
+
+
         }
 
         if(IsServer && IsSpawned && inputDirection != Vector2.zero)
@@ -68,6 +124,7 @@ public class PlayerController : NetworkBehaviour
             //We dont need to worry about calling a client rpc because the Network Object is responsible for syncing the transfrom properties
             Vector3 move=new Vector3(inputDirection.x,0,inputDirection.y);
             transform.position += move * playerSpeed * Time.deltaTime;
+
         }
     }
 
@@ -75,5 +132,32 @@ public class PlayerController : NetworkBehaviour
     private void SendInputServerRpc(Vector2 input)
     {
         inputDirection = input;
+    }
+
+    [Rpc(SendTo.Server)]
+    private void SendInfoRpc(int incomingScore)
+    {
+        score.Value = incomingScore;
+       /* var newData = data.Value;
+        newData.health -= 10;
+        newData.mana -= 10;
+        data.Value = newData;*/
+        
+    }
+
+    [Rpc(SendTo.Server)]
+    void SpawnObjectRpc()
+    {
+        spawnedObject.GetComponent<NetworkObject>().Spawn();
+    }
+
+    private void DisplayScore(int previousVal, int newVal)
+    {
+        Debug.Log($"The player's score has changed from {previousVal} to {newVal}");
+    }
+
+    private void DisplayData(PlayerCustomData previousVal, PlayerCustomData newVal)
+    {
+        Debug.Log($"The player's healt has changed from {previousVal.health} to {newVal.health}");
     }
 }
